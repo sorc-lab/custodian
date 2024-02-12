d9fba3c2b2e572d6fce9647a0fb5e16f7d6eae06
macfisher <macfisher01@gmail.com>
2024-02-11 20:34:23 -0700

    initial commit

---
+++ src/main/java/com/sorclab/custodian/CustodianApplication.java
@@ -0,0 +1,13 @@
+package com.sorclab.custodian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustodianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustodianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sorclab/custodian/CustodianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorclab.custodian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustodianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
