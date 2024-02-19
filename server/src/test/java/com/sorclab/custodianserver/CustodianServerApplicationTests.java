424e135bad93255408d85f7dc7b4471097d36440
macfisher <macfisher01@gmail.com>
2024-02-19 09:26:04 -0700

    code complete w/ TODOs

--- server/src/test/java/com/sorclab/custodianserver/CustodianServerApplicationTests.java
+++ server/src/test/java/com/sorclab/custodianserver/CustodianServerApplicationTests.java
@@ -3,11 +3,14 @@ package com.sorclab.custodianserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SuppressWarnings("unused")
 @SpringBootTest
 class CustodianServerApplicationTests {
-//    @Test
-//    void contextLoads() {
-//
-//    }
+    @Test
+    void contextLoads() {
+
+    }
+
+    // TODO: Write full e2e test for filesystem sync w/ expected json files
+    // TODO: Write repository tests in separate suite w/ h2.
+    // TODO: Write unit tests.
 }
