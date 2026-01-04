b25d062e005bff61658779339232b806853afe92
Mackenzie Fisher <macfisher01@gmail.com>
2026-01-04 15:06:59 -0700

    Cus 13 (#21)
    * CUS-13: rm task by id
    
    * CUS-13: test suite md notes file

--- cport/task_repo.c
+++ cport/task_repo.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LINE_SIZE 1024
+#define DB "tasks.tsv"
+#define TMP_DB "tasks.tmp"
 
-static const char DB[] = "tasks.tsv";
+#define MAX_LINE_SIZE 1024
 
 void task_save(task_t* task) {
     if (!task) return;
@@ -27,18 +28,66 @@ void task_save(task_t* task) {
     fclose(fp);
 }
 
-// generate sequential primary key id based on last generated
-static long task_gen_seq_id() {
+// stream DB into TMP_DB, excluding the target_id record, then replicate orig. DB w/ tmp & cleanup
+void task_delete_by_id(long target_id) {
+    FILE* db = task_read_db();
+    FILE* tmp_db = fopen(TMP_DB, "w");
+    if (!tmp_db) {
+        fclose(db);
+        fprintf(stderr, "Failed to open TEMP Task database for write.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[MAX_LINE_SIZE];
+    bool found = false;
+
+    while (fgets(line, sizeof(line), db)) {
+        long id = 0;
+        if (sscanf(line, "%ld", &id) == 1 && id == target_id) {
+            found = true;
+            continue; // target to delete found, skip writing this record
+        }
+        fputs(line, tmp_db);
+    }
+
+    fclose(db);
+    fclose(tmp_db);
+
+    if (!found) {
+        remove(TMP_DB);
+        fprintf(stderr, "Failed to delete. Could not find Task ID: %ld.\n", target_id);
+        exit(EXIT_FAILURE);
+    }
+
+    if (remove(DB) != 0) {
+        fprintf(stderr, "Failed to remove original Task database.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (rename(TMP_DB, DB) != 0) {
+        fprintf(stderr, "Failed to replicate Task database.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static FILE* task_read_db() {
     FILE* fp = fopen(DB, "r");
     if (!fp) {
         fprintf(stderr, "Failed to open Task database for read.\n");
         exit(EXIT_FAILURE);
     }
 
+    return fp;
+}
+
+// generate sequential primary key id based on last generated
+static long task_gen_seq_id() {
+    FILE* db = task_read_db();
+
     long last_id = 0;
     char line[MAX_LINE_SIZE];
 
-    while (fgets(line, sizeof(line), fp)) {
+    while (fgets(line, sizeof(line), db)) {
         long id = 0;
         if (sscanf(line, "%ld", &id) == 1) {
             if (id > last_id) {
@@ -47,6 +96,6 @@ static long task_gen_seq_id() {
         }
     }
 
-    fclose(fp);
+    fclose(db);
     return last_id + 1;
 }
